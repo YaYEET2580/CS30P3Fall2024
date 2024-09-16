357d9d0a597fe20e4d2c23d7671262b420ec1f26
37168004 <37168004@L815WD220030.edu.cbe.ab.ca>
2024-09-16 13:50:50 -0600

    1:50 AM

--- Chapter10/src/Examples/Demo2.java
+++ Chapter10/src/Examples/Demo2.java
@@ -10,6 +10,7 @@ import java.awt.Color;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
+import javax.swing.ImageIcon;
 import javax.swing.JTextArea;
 import javax.swing.JLabel;
 import javax.swing.JDesktopPane;
@@ -17,6 +18,7 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JSlider;
 
 public class Demo2 {
 
@@ -51,13 +53,19 @@ public class Demo2 {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		
+		ImageIcon crescentHeights = new ImageIcon("C:\\Users\\37168004\\git\\CS30P3Fall2024\\Chapter10\\src\\Images\\images.png");
+		ImageIcon lesterBPearson = new ImageIcon("C:\\Users\\37168004\\git\\CS30P3Fall2024\\Chapter10\\src\\Images\\865-logo.png");
+		ImageIcon westernCanada = new ImageIcon("C:\\Users\\37168004\\git\\CS30P3Fall2024\\Chapter10\\src\\Images\\816-logo.png");
+		
+		
 		frame = new JFrame();
-		frame.setBounds(100, 100, 661, 494);
+		frame.setBounds(100, 100, 745, 494);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
 		JPanel panel = new JPanel();
-		panel.setBounds(10, 11, 625, 433);
+		panel.setBounds(10, 11, 709, 433);
 		frame.getContentPane().add(panel);
 		panel.setLayout(null);
 		
@@ -95,17 +103,17 @@ public class Demo2 {
 		
 		
 		JComboBox grades = new JComboBox();
-		grades.setModel(new DefaultComboBoxModel(new String[] {"*Select grade*", "10", "11", "12"}));
+		grades.setModel(new DefaultComboBoxModel(new String[] {"Select grade", "10", "11", "12"}));
 		grades.setBounds(0, 64, 132, 42);
 		panel.add(grades);
 		
 		JComboBox schools = new JComboBox();
-		schools.setModel(new DefaultComboBoxModel(new String[] {"*Select school*", "Crescent Heights High School", "Lester B. Pearson High School", "Western Canada High School"}));
-		schools.setBounds(163, 64, 174, 42);
+		schools.setModel(new DefaultComboBoxModel(new String[] {"Select school", "Crescent Heights High School", "Lester B. Pearson High School", "Western Canada High School"}));
+		schools.setBounds(163, 64, 197, 42);
 		panel.add(schools);
 		
 		JTextArea display = new JTextArea();
-		display.setBounds(0, 119, 337, 114);
+		display.setBounds(0, 119, 444, 114);
 		panel.add(display);
 		
 		JLabel images = new JLabel("");
@@ -114,7 +122,44 @@ public class Demo2 {
 		
 		JButton submitButton = new JButton("SUBMIT");
 		submitButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
+			public void actionPerformed(ActionEvent e) 
+			{
+				String grade = " ";
+				String school = " ";
+				String firstName = firstNameTextField.getText();
+				String lastName = lastNameTextField.getText();
+				
+				if(grades.getSelectedItem().equals("10")) {
+					grade = "10";
+				}
+				else if(grades.getSelectedItem().equals("11")) {
+					grade = "11";
+				}
+				else if(grades.getSelectedItem().equals("12")) {
+					grade = "12";
+				}
+				
+				if(schools.getSelectedItem().equals("Crescent Heights High School")) {
+					school = "Crescent Heights High School";
+					images.setIcon(crescentHeights);
+				}
+				else if(schools.getSelectedItem().equals("Lester B. Pearson High School")) {
+					school = "Lester B. Pearson High School";
+					images.setIcon(lesterBPearson);
+				}
+				else if(schools.getSelectedItem().equals("Western Canada High School")) {
+					school = "Western Canada High School";
+					images.setIcon(westernCanada);
+				}
+				
+				display.setText
+				(
+						firstName + " " +
+						lastName + " is in grade " +
+						grade + " and goes to " + 
+						school + "."
+						
+				);
 				
 				
 			}
@@ -122,7 +167,7 @@ public class Demo2 {
 		submitButton.setFont(new Font("Tahoma", Font.PLAIN, 25));
 		submitButton.setFont(new Font("Tahoma", Font.PLAIN, 25));
 		submitButton.setForeground(Color.BLUE);
-		submitButton.setBounds(370, 27, 245, 221);
+		submitButton.setBounds(454, 27, 245, 221);
 		panel.add(submitButton);
 	}
 }
