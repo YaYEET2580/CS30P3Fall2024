013c9e22325fba861f0f913c0c1f798a19da60cf
37168004 <37168004@L815WD220030.edu.cbe.ab.ca>
2024-10-08 15:31:56 -0600

    3:31 PM

---
+++ Chapter11/src/Skillbuilders/Roster.java
@@ -0,0 +1,18 @@
+package Skillbuilders;
+
+import java.io.*;
+public class Roster {
+
+	public static void main(String[] args) {
+		File dataFile;
+		FileReader in;
+		String fileName;
+		BufferedReader readFile;
+		FileWriter out;
+		BufferedWriter writeFile;
+		
+		
+
+	}
+
+}
--- Chapter11/src/Skillbuilders/Stats2.java
+++ Chapter11/src/Skillbuilders/Stats2.java
@@ -1,9 +1,100 @@
 package Skillbuilders;
 
+import java.io.*;
+import java.text.NumberFormat;
+import java.util.*;
+
 public class Stats2 {
 	
 	public static void main(String[] args) {
+		File dataFile;
+		FileReader in;
+		String fileName;
+		BufferedReader readFile;
+		FileWriter out;
+		BufferedWriter writeFile;
+		int numberStudents;
+		String studentName, score;
+		double scoreValue;
+		double lowScore = 100;
+		double highScore = 0;
+		double avgScore;
+		double totalScores = 0;
+		int numScore = 0;
+		NumberFormat percent = NumberFormat.getPercentInstance();
+		Scanner input = new Scanner(System.in);
 		
+		System.out.print("Enter file name for storing names and grades: ");
+		fileName = input.next();
+		
+		System.out.print("How many students: ");
+		numberStudents = input.nextInt();
+		
+		try {
+			dataFile = new File(fileName);
+			out = new FileWriter(dataFile);
+			writeFile = new BufferedWriter(out);
+			
+			for (int i = 0; i < numberStudents; i++) {
+				System.out.print("Enter student name: ");
+				studentName = input.next();
+				
+				System.out.print("Enter test grade: ");
+				score = input.next();
+				
+				writeFile.write(studentName);
+				writeFile.newLine();
+				writeFile.write(score);
+				writeFile.newLine();
+			}
+			writeFile.close();
+			out.close();
+			System.out.println("The data that you have written is now in the file.");
+		} 
+		catch (IOException e) {
+			System.out.println("An error has occured.");
+			System.err.println("IOException: " + e.getMessage());
+		}
+		
+		try {
+			dataFile = new File(fileName);
+			in = new FileReader(dataFile);
+			readFile = new BufferedReader(in);
+			
+			while ((studentName = readFile.readLine()) != null) {
+				score = readFile.readLine();
+				System.out.println(studentName + " " + score);
+				numScore += 1;
+				scoreValue = Double.parseDouble(score);
+				totalScores = totalScores + scoreValue;
+				
+				if (scoreValue < lowScore) {
+					lowScore = scoreValue;
+				}
+				if (scoreValue > highScore) {
+					highScore = scoreValue;
+				}
+				
+			}
+			avgScore = totalScores / numScore;
+			
+			System.out.println("Low Score = " + percent.format(lowScore/100));
+			System.out.println("High Score = " + percent.format(highScore/100));
+			System.out.println("Average = " + percent.format(avgScore/100));
+			
+			in.close();
+		}
+		catch (FileNotFoundException e) 
+        {
+			System.out.println("File does not exist or could not be found.");
+			System.err.println("FileNotFoundException: " + e.getMessage());
+
+        } 
+		catch (IOException e) {
+			System.out.println("An error has occured.");
+			System.err.println("IOException: " + e.getMessage());
+		}
+		input.close();
 	}
 
 }
