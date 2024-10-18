9a81dd042f3375c5ea087957cedb50fad938464c
37168004 <37168004@L815WD220030.edu.cbe.ab.ca>
2024-10-18 11:45:46 -0600

    11:45 PM

---
+++ Chapter11/src/Mastery/FindAndReplace.java
@@ -0,0 +1,33 @@
+package Mastery;
+
+import java.io.*;
+import java.util.*;
+
+public class FindAndReplace {
+
+	public static void main(String[] args) {
+		File dataFile;
+		FileReader in;
+		String fileName;
+		BufferedReader readFile;
+		FileWriter out;
+		BufferedWriter writeFile;
+		String oldPhrase;
+		String newPhrase;
+		String line;
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter the file name: ");
+		fileName = input.nextLine();
+		
+		System.out.print("Enter in the old word: ");
+	    oldPhrase = input.nextLine();
+
+	    System.out.print("Enter in the new word: ");
+	    newPhrase = input.nextLine();
+	    
+
+
+	}
+
+}
