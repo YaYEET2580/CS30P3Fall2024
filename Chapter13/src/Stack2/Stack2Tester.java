21a98974c6dd80eb1701abf071cc633f498b8f50
37168004 <37168004@L815WD220030.edu.cbe.ab.ca>
2024-11-18 13:59:30 -0700

    1:59 PM

--- Chapter13/src/Stack2/Stack2Tester.java
+++ Chapter13/src/Stack2/Stack2Tester.java
@@ -3,7 +3,16 @@ package Stack2;
 public class Stack2Tester {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Stack2 s2 = new Stack2(5);
+		
+		s2.push("Red");
+		s2.push("Green");
+		s2.push("Yellow");
+		System.out.println("Top of Stack s2: " + s2.top());
+		System.out.println("Items in Stack s2: " + s2.size());
+		s2.pop();
+		System.out.println("Top of Stack s2: " + s2.top());
+		System.out.println("Items in Stack s2: " + s2.size());
 
 	}
 
--- Chapter8/src/UEmployee_MASTERY/testUEmployee.java
+++ Chapter8/src/UEmployee_MASTERY/testUEmployee.java
@@ -3,7 +3,10 @@ package UEmployee_MASTERY;
 public class testUEmployee {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Staff universityStaffMember = new Staff("John", "Smith", 85174.00, "Professor");
+		Faculty univeristyFacultyMember = new Faculty("Jane", "Doe", 95000.00, "Computer Science");
+		
+		System.out.println("Which Univeristy employee");
 
 	}
 
