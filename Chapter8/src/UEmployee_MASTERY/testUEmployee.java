24bf174f3c1095cec6c588b07f361b84d23b40f9
37168004 <37168004@L815WD220030.edu.cbe.ab.ca>
2024-11-22 11:47:30 -0700

    11:47 AM

--- Chapter8/src/UEmployee_MASTERY/testUEmployee.java
+++ Chapter8/src/UEmployee_MASTERY/testUEmployee.java
@@ -1,12 +1,26 @@
 package UEmployee_MASTERY;
 
+import java.util.*;
+
 public class testUEmployee {
 
 	public static void main(String[] args) {
-		Staff universityStaffMember = new Staff("John", "Smith", 85174.00, "Professor");
-		Faculty univeristyFacultyMember = new Faculty("Jane", "Doe", 95000.00, "Computer Science");
+		Staff universityStaffMember1 = new Staff("John", "Smith", 85174.00, "Professor");
+		Faculty universityFacultyMember1 = new Faculty("Jane", "Doe", 95000.00, "Computer Science");
+		Scanner input = new Scanner(System.in);
+		int optionMember;
+		int optionChoice;
 		
-		System.out.println("Which Univeristy employee");
+		System.out.println("University employees:");
+		System.out.println("1. " + universityStaffMember1.getFirstName() + " " + universityStaffMember1.getLastName());
+		System.out.println("2. " + universityFacultyMember1.getFirstName() + " " + universityFacultyMember1.getLastName());
+		System.out.print("Which Univeristy employee would you like to see their info?: ");
+		optionMember = input.nextInt();
+		
+		
+		
+		
+		
 
 	}
 
