1292d572e04dff52d9ddb6c6e1a85f9a2475b413
37168004 <37168004@L815WD220030.edu.cbe.ab.ca>
2025-01-06 13:58:41 -0700

    1:58 PM

--- RoverProject/src/AvoidObstacles/AvoidObstacles3.java
+++ RoverProject/src/AvoidObstacles/AvoidObstacles3.java
@@ -1,5 +1,54 @@
 package AvoidObstacles;
 
+import com.phidget22.*;
+
 public class AvoidObstacles3 {
+	public static void main(String[] args) throws Exception {
+
+        //Connect to wireless rover
+        Net.addServer("", "192.168.100.1", 5661, "", 0);
+
+        //Create
+        DCMotor leftMotors = new DCMotor();
+        DCMotor rightMotors = new DCMotor();
+        DistanceSensor sonar = new DistanceSensor();
+
+        //Address
+        leftMotors.setChannel(0);
+        rightMotors.setChannel(1);
+
+        //Open
+        leftMotors.open(5000);
+        rightMotors.open(5000);
+        sonar.open(5000);
+
+        while (true) {
+
+            System.out.println("Distance: " + sonar.getDistance() + " mm");
+            
+            if (sonar.getDistance() < 200) {
+                //Object detected! Stop motors and rotate
+                leftMotors.setTargetVelocity(0);
+                rightMotors.setTargetVelocity(0);
+                
+                leftMotors.setTargetVelocity(0.25);
+                rightMotors.setTargetVelocity(-0.25);
+                Thread.sleep((int) (6000 * Math.random() + 1000));
+                
+                leftMotors.setTargetVelocity(0);
+                rightMotors.setTargetVelocity(0);
+                Thread.sleep(500);
+                
+
+            } else {
+                //Move forward slowly (25% max speed)
+                leftMotors.setTargetVelocity(0.25);
+                rightMotors.setTargetVelocity(0.25);
+            }
+
+            //Wait for 250milliseconds
+            Thread.sleep(250);
+        }
+    }
 
 }
--- RoverProject/src/Challenge/Challenge.java
+++ RoverProject/src/Challenge/Challenge.java
@@ -1,5 +1,113 @@
 package Challenge;
 
+import com.phidget22.*;
+
 public class Challenge {
+	public static void main(String[] args) throws Exception {
+	     
+		//Connect to wireless rover
+		Net.addServer("", "192.168.100.1", 5661, "", 0);
+	     
+		//Create
+	     DCMotor leftMotors = new DCMotor();
+	     DCMotor rightMotors = new DCMotor();
+	     
+	     //Address
+	     leftMotors.setHubPort(5);
+	     leftMotors.setChannel(0);
+	     rightMotors.setHubPort(5);
+	     rightMotors.setChannel(1);
+	     
+	     //Open
+	     leftMotors.open(7000);
+	     rightMotors.open(7000);
+	     
+	     
+	     //Move forward at full speed
+	     leftMotors.setTargetVelocity(1);
+	     rightMotors.setTargetVelocity(1);
+	     //Wait for 1 second
+	     Thread.sleep(1050);
+	     
+	   //Full stop
+	     leftMotors.setTargetVelocity(0);
+	     rightMotors.setTargetVelocity(0);
+	   //Wait for 1 second
+	     Thread.sleep(1000);
+	     
+	     //Turn
+	     leftMotors.setTargetVelocity(0);
+	     rightMotors.setTargetVelocity(1);
+	     //Wait for 505milliseconds
+	     Thread.sleep(710);
+	     
+	   //Full stop
+	     leftMotors.setTargetVelocity(0);
+	     rightMotors.setTargetVelocity(0);
+	   //Wait for 1 second
+	     Thread.sleep(1000);
+	    
+	   //Move forward at full speed
+	     leftMotors.setTargetVelocity(1);
+	     rightMotors.setTargetVelocity(1);
+	     //Wait for 1 second
+	     Thread.sleep(1050);
+	     
+	   //Full stop
+	     leftMotors.setTargetVelocity(0);
+	     rightMotors.setTargetVelocity(0);
+	   //Wait for 1 second
+	     Thread.sleep(1000);
+	    
+	     //Turn
+	     leftMotors.setTargetVelocity(0);
+	     rightMotors.setTargetVelocity(1);
+	   //Wait for 505milliseconds
+	     Thread.sleep(710);
+	     
+	   //Full stop
+	     leftMotors.setTargetVelocity(0);
+	     rightMotors.setTargetVelocity(0);
+	   //Wait for 1 second
+	     Thread.sleep(1000);
+	    
+	     leftMotors.setTargetVelocity(1);
+	     rightMotors.setTargetVelocity(1);
+	     //Wait for 1 second
+	     Thread.sleep(1050);
+	     
+	   //Full stop
+	     leftMotors.setTargetVelocity(0);
+	     rightMotors.setTargetVelocity(0);
+	   //Wait for 1 second
+	     Thread.sleep(1000);
+	    
+	   //Turn
+	     leftMotors.setTargetVelocity(0);
+	     rightMotors.setTargetVelocity(1);
+	   //Wait for 505milliseconds
+	     Thread.sleep(710);
+	     
+	   //Full stop
+	     leftMotors.setTargetVelocity(0);
+	     rightMotors.setTargetVelocity(0);
+	   //Wait for 1 second
+	     Thread.sleep(1000);
+	    
+	     leftMotors.setTargetVelocity(1);
+	     rightMotors.setTargetVelocity(1);
+	     //Wait for 1 second
+	     Thread.sleep(1050);
+	     
+	   //Full stop
+	     leftMotors.setTargetVelocity(0);
+	     rightMotors.setTargetVelocity(0);
+	   //Wait for 1 second
+	     Thread.sleep(1000);
+	    
+	    
+	    
+	 }
+
 
 }
